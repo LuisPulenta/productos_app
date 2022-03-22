e296e518d3a77fbfb067a7b6fafecab847fc9510
LuisPulenta <luisalbertonu@gmail.com>
2022-03-22 13:36:04 -0300

    Video 215

---
+++ lib/widgets/auth_background.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+class AuthBackGround extends StatelessWidget {
+  final Widget child;
+
+  const AuthBackGround({Key? key, required this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Stack(
+        children: [
+          _PurpleBox(),
+          HeaderIcon(),
+          child,
+        ],
+      ),
+    );
+  }
+}
+
+class HeaderIcon extends StatelessWidget {
+  const HeaderIcon({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        margin: EdgeInsets.only(top: 30),
+        width: double.infinity,
+        child: Icon(
+          Icons.person_pin,
+          color: Colors.white,
+          size: 100,
+        ),
+      ),
+    );
+  }
+}
+
+class _PurpleBox extends StatelessWidget {
+  const _PurpleBox({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      width: double.infinity,
+      height: size.height * 0.4,
+      decoration: _purpleBackground(),
+      child: Stack(
+        children: [
+          Positioned(
+            child: _Bubble(),
+            top: 90,
+            left: 30,
+          ),
+          Positioned(
+            child: _Bubble(),
+            top: -40,
+            left: -30,
+          ),
+          Positioned(
+            child: _Bubble(),
+            top: -50,
+            right: -20,
+          ),
+          Positioned(
+            child: _Bubble(),
+            bottom: -50,
+            left: 10,
+          ),
+          Positioned(
+            child: _Bubble(),
+            bottom: 120,
+            right: 20,
+          ),
+        ],
+      ),
+    );
+  }
+
+  BoxDecoration _purpleBackground() => BoxDecoration(
+          gradient: LinearGradient(colors: [
+        Color.fromRGBO(63, 63, 156, 1),
+        Color.fromRGBO(90, 70, 178, 0.9),
+      ]));
+}
+
+class _Bubble extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100),
+        color: Color.fromRGBO(255, 255, 255, 0.06),
+      ),
+    );
+  }
+}
